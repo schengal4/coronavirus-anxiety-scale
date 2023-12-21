9a3f845de0e03394f8d2c37b1fe46629ad7c7352
Venkata Sai Suraj Chengalvala <55936921+schengal4@users.noreply.github.com>
2023-12-21 09:19:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ def main():
     st.title("Coronavirus Anxiety Scale (CAS)")
     st.write("**Instructions**: Please indicate how often you've experienced the following over the *last 2 weeks*.")
 
-    # Define the questions
-
+    # Questions and corresponding options
     options = ['Not at all', 'Rare, less than a day or two', 'Several days', 'More than 7 days', 'Nearly every day over the last 2 weeks']
     questions = [
         "I felt dizzy, lightheaded, or faint when I read or listened to news about the coronavirus.",
@@ -14,19 +13,20 @@ def main():
         "I lost interest in eating when I thought about or was exposed to information about the coronavirus.",
         "I felt nauseous or had stomach problems when I thought about or was exposed to information about the coronavirus."
     ]
-
+    
+    # Displays drop down menus allowing users to select their answwers to the questions.
     responses = [st.selectbox(q, options) for q in questions]
 
-    # Map the responses to scores (0-4)
+    # Map the responses to scores (0-4) and calculate total score.
     scores = [options.index(r) for r in responses]
     total_score = sum(scores)
     
     # Submit button
     if st.button('Submit'):
         if total_score >= 9:
-            st.write("Your score is", total_score, ". This indicates dysfunctional coronavirus-related anxiety. Please consult a mental health professional.")
+            st.write("Your CAS score is", total_score, ". This indicates dysfunctional coronavirus-related anxiety. Please consult a mental health professional.")
         else:
-            st.write("Your score is", total_score, ". This doesn't indicate dysfunctional coronavirus-related anxiety. \
+            st.write("Your CAS score is", total_score, ". This doesn't indicate dysfunctional coronavirus-related anxiety. \
             But, if you feel something's wrong, you should still consult a mental health professional.")
 if __name__ == "__main__":
     main()
